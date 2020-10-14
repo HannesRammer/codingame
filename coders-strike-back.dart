3ab24ca4b51aeb33b92ab253892177318c66b816
Hannes Rammer <Hannes.Rammer@gmail.com>
2020-10-15 00:45:05 +0200

    gold 1019

--- coders-strike-back.dart
+++ coders-strike-back.dart
@@ -5,80 +5,166 @@ import 'dart:math';
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
-List lastPosition = [0,0];
-int thrust=0;
-List vXY = [0,0];
-void main() {
-    List inputs;
-    var boost = 0;
-    // game loop
-    while (true) {
+Map<int, Position> checkpoints = {};
 
-        inputs = stdin.readLineSync().split(' ');
-        int x = int.parse(inputs[0]);
-        int y = int.parse(inputs[1]);
-        vXY = [x-lastPosition[0], y-lastPosition[1]];
-        stderr.writeln("vXY $vXY");
-        lastPosition = [x,y];
-        int nextCheckpointX = int.parse(inputs[2]); // x position of the next check point
-        int nextCheckpointY = int.parse(inputs[3]); // y position of the next check point
-        int nextCheckpointDist = int.parse(inputs[4]); // distance to the next checkpoint
-        int nextCheckpointAngle = int.parse(inputs[5]); // angle between your pod orientation and the direction of the next checkpoint
-        inputs = stdin.readLineSync().split(' ');
-        int opponentX = int.parse(inputs[0]);
-        int opponentY = int.parse(inputs[1]);
+class Unit {
+  int id;
+  int x;
+  int y;
+  int vx;
+  int vy;
+  int angle;
+  int nextCheckpointId;
+  Position nextCheckpoint;
+  Position calcCheckpoint;
+  Position position;
 
-        nextCheckpointX = nextCheckpointX - vXY[0];
-        nextCheckpointY = nextCheckpointY - vXY[1];
-        // Write an action using print()
-        // To debug: stderr.writeln('Debug messages...');
-        int t = (nextCheckpointDist/10).toInt();
+  Unit(this.id, this.x, this.y, this.vx, this.vy, this.angle, this.nextCheckpointId, this.nextCheckpoint, this.calcCheckpoint, this.position);
 
-
-        stderr.writeln('nextCheckpointAngle:${nextCheckpointAngle}');
-        stderr.writeln('nextCheckpointDist:${nextCheckpointDist}');
-        if (nextCheckpointDist <= 1000){
-
-            thrust = 50;
-
-
-        }else{
-            thrust=100;
-        }
-        if(nextCheckpointAngle >100 || nextCheckpointAngle <-100){
-            thrust=0;//(thrust/10).toInt();
-        }
-
-
-        // You have to output the target position
-        // followed by the power (0 <= thrust <= 100)
-        // i.e.: "x y thrust"
-        if(nextCheckpointDist > 6000 && nextCheckpointAngle <4 && nextCheckpointAngle >-4){
-            print("${nextCheckpointX} ${nextCheckpointY} BOOST 100");
-
-        }else{
-            if(boost==0){
-                print("${nextCheckpointX} ${nextCheckpointY} BOOST BOOSTTWO");
-                boost=1;
-            }else{
-                /*if(nextCheckpointAngle > -50 && nextCheckpointAngle < 50){
-                    thrust = (100 - (((nextCheckpointAngle).abs()).abs()).toInt());
-
-                }else{
-                    thrust = 70;
-                }
-                if(nextCheckpointAngle > -10 && nextCheckpointAngle < 10){
-                    thrust = (100 - ((nextCheckpointAngle).abs()).abs()*1);
-
-                }*/
-                //print("${nextCheckpointX} ${nextCheckpointY} $thrust THRUST $thrust");
-                print("${nextCheckpointX} ${nextCheckpointY} 100 THRUST $thrust");
-            }
-        }
-
-    }
+  String toString() =>
+          "Unit => Id: $id x: $x  y: $y  vx: $vx  vy: $vy  angle: $angle nextCheckpointId $nextCheckpointId  nextCheckpoint: $nextCheckpoint  nextCalcCheckpoint: $calcCheckpoint Position: $position";
 }
 
-double getDistance(int xA, int yA, int xB, int yB) {
-    return sqrt((pow((xA - xB), 2) + pow((yA - yB), 2)));
+class Position {
+  int x;
+  int y;
+
+  Position(this.x, this.y);
+
+  String toString() => '$x $y';
+
+  @override
+  int get hashCode {
+    int result = 17;
+    result = 37 * result + x;
+    result = 37 * result + y;
+    return result;
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    if (other is! Position) return false;
+    return other.x == x && other.y == y;
+  }
+
+//  num distance(Position p) => (x - p.x).abs() + (y - p.y).abs();
+  num distance(Position p) => sqrt((pow((x - p.x), 2) + pow((y - p.y), 2)));
+}
+
+List<Unit> myUnits = [];
+List<Unit> opponentUnits = [];
+
+void main() {
+  List inputs;
+  int laps = int.parse(stdin.readLineSync());
+  int checkpointCount = int.parse(stdin.readLineSync());
+  for (int i = 0; i < checkpointCount; i++) {
+    inputs = stdin.readLineSync().split(' ');
+    int checkpointX = int.parse(inputs[0]);
+    int checkpointY = int.parse(inputs[1]);
+    Position checkpointPos = Position(checkpointX, checkpointY);
+
+    checkpoints[i] = checkpointPos;
+  }
+  int boost = 0;
+  // game loop
+  while (true) {
+    myUnits.clear();
+    opponentUnits.clear();
+    // Read your Units
+    for (int i = 0; i < 2; i++) {
+      inputs = stdin.readLineSync().split(' ');
+      int x = int.parse(inputs[0]); // x position of your pod
+      int y = int.parse(inputs[1]); // y position of your pod
+      int vx = int.parse(inputs[2]); // x speed of your pod
+      int vy = int.parse(inputs[3]); // y speed of your pod
+      int angle = int.parse(inputs[4]); // angle of your pod
+      int nextCheckPointId = int.parse(inputs[5]); // next check point id of your pod
+      Position position = Position(x, y);
+      Position nextCheckpoint = checkpoints[nextCheckPointId]; // x position of the next check point
+      int nextCheckpointX = nextCheckpoint.x; // x position of the next check point
+      int nextCheckpointY = nextCheckpoint.y; // y position of the next check point
+
+      Position calcCheckpoint = Position(nextCheckpointX - vx, nextCheckpointY - vy);
+      myUnits.add(Unit(i, x, y, vx, vy, angle, nextCheckPointId, nextCheckpoint, calcCheckpoint, position));
+    }
+    // Read opponent Units
+    for (int i = 0; i < 2; i++) {
+      inputs = stdin.readLineSync().split(' ');
+      int x2 = int.parse(inputs[0]); // x position of the opponent's pod
+      int y2 = int.parse(inputs[1]); // y position of the opponent's pod
+      int vx2 = int.parse(inputs[2]); // x speed of the opponent's pod
+      int vy2 = int.parse(inputs[3]); // y speed of the opponent's pod
+      int angle2 = int.parse(inputs[4]); // angle of the opponent's pod
+      int nextCheckPointId2 = int.parse(inputs[5]); // next check point id of the opponent's pod
+      Position position2 = Position(x2, y2);
+      Position nextCheckpoint2 = checkpoints[nextCheckPointId2]; // x position of the next check point
+      int nextCheckpointX2 = nextCheckpoint2.x; // x position of the next check point
+      int nextCheckpointY2 = nextCheckpoint2.y;
+      // y position of the next check point
+
+      Position calcCheckpoint2 = Position(nextCheckpointX2 - vx2, nextCheckpointY2 - vy2);
+      opponentUnits.add(Unit(i, x2, y2, vx2, vy2, angle2, nextCheckPointId2, nextCheckpoint2, calcCheckpoint2, position2));
+    }
+
+    // Write an action using print()
+    // To debug: stderr.writeln('Debug messages...');
+
+    // You have to output the target position
+    // followed by the power (0 <= thrust <= 100)
+    // i.e.: "x y thrust"
+    int thrust = 100;
+    for (var i = 0; i < myUnits.length; ++i) {
+      Unit unit = myUnits[i];
+      num nextCheckpointDist = unit.position.distance(unit.nextCheckpoint);
+      stderr.writeln("nextCheckpointDist:${nextCheckpointDist} ");
+      if (nextCheckpointDist <= 1500) {
+        thrust = 100;
+      } else {
+        thrust = 100;
+      }
+      //if (unit.angle > 100 || unit.angle < -100) {
+      //  thrust = 30;//thrust = 0;
+      //}
+
+      Position cC = unit.calcCheckpoint;
+      bool shield = false;
+      stderr.writeln("id:${unit.id} angle: ${unit.angle}");
+      for (Unit opponent in opponentUnits) {
+        if (opponent.position.distance(unit.position) <= 1000) {
+          shield = true;
+        }
+      }
+      Position closestOpponent = getClosestPos(unit.position, opponentUnits);
+      if (false && i == 1) {
+        print("${closestOpponent.x} ${closestOpponent.y} 100 closestOpponent 100");
+      } else {
+        if (shield && (unit.vx > 200 || unit.vy > 200)) {
+          print("${cC.x} ${cC.y} SHIELD SHIELD");
+        } else {
+          if (boost == 0) {
+            print("${cC.x} ${cC.y} BOOST BOOST");
+            boost = 1;
+          } else {
+            print("${cC.x} ${cC.y} $thrust THRUST $thrust");
+          }
+        }
+      }
+    }
+  }
+}
+
+Position getClosestPos(Position myPos, List<Unit> units) {
+  num shortestDistance = -1;
+  Position targetField = null;
+  for (int i = 0; i < units.length; i++) {
+    Unit u = units[i];
+    Position pos = u.position;
+    num distance = pos.distance(myPos);
+    if ((distance < shortestDistance || shortestDistance == -1)) {
+      shortestDistance = distance;
+      targetField = pos;
+    }
+  }
+  return targetField;
 }
